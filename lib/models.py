53119ca690b16e4876daa2994464ab0c12fb4958
gladys <achandogladys6@gmail.com>
2025-08-29 02:37:41 +0300

    patient class

--- lib/models.py
+++ lib/models.py
@@ -3,3 +3,14 @@ from sqlalchemy.orm import relationship
 from .database import Base
 from datetime import date
 
+class Patient(Base):
+    __tablename__ = "patients"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String)
+    contact = Column(String)
+    join_date = Column(Date, default=date.today)
+    appointments = relationship("Appointment", back_populates="patient")
+
+    
