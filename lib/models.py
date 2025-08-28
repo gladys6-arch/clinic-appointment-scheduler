16bbc08b26b815398ac94220635a19482def877f
gladys <achandogladys6@gmail.com>
2025-08-29 02:38:42 +0300

    doctors class

--- lib/models.py
+++ lib/models.py
@@ -13,4 +13,11 @@ class Patient(Base):
     join_date = Column(Date, default=date.today)
     appointments = relationship("Appointment", back_populates="patient")
 
-    
+class Doctor(Base):
+    __tablename__ = "doctors"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    specialty = Column(String)
+    hire_date = Column(Date, default=date.today)
+    phone = Column(String)
+    appointments = relationship("Appointment", back_populates="doctor")
