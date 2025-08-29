99fd9479765a30102b3ca213666a195e3b84f20b
gladys <achandogladys6@gmail.com>
2025-08-29 03:48:21 +0300

    pipfile

--- lib/cli.py
+++ lib/cli.py
@@ -1,6 +1,6 @@
 import click
 
-from .crud import (
+from lib.crud import (
   add_patient, get_patients_list, get_patients_dict,
     add_doctor, get_doctors_list,
     schedule_appointment, get_appointments_list
